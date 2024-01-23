f052bdcecaa9c1b64e6d36adeb874a874fd8b9bd
j-w-park <okml133@gmail.com>
2024-01-23 21:14:30 +0900

    refactor: refetchInterval을 웹소켓 이벤트로 대체

--- packages/web/src/utils/websocket/provider.tsx
+++ packages/web/src/utils/websocket/provider.tsx
@@ -1,14 +1,23 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
 import Context from './context';
+import { useQueryClient } from '@tanstack/react-query';
+import { Timer } from '@/api/types';
+import { AxiosResponse } from 'axios';
+
+interface WebSocketMessage<T = unknown> {
+  readonly type:
+    | 'TIMER_CREATED'
+    | 'TIMER_DELETED'
+    | 'TIMER_TICK'
+    | 'TIMER_PAUSED'
+    | 'TIMER_STARTED';
+  readonly data: T;
+}
 
 const maxRetries = 5; // 최대 재시도 횟수
 
-interface Props {
-  readonly onMessage: WebSocket['onmessage'];
-}
-
-export const WebSocketProvider = (props: Props) => {
+export const WebSocketProvider = () => {
   const params = useParams();
 
   const connRef = useRef<WebSocket | null>(null);
@@ -17,11 +26,83 @@ export const WebSocketProvider = (props: Props) => {
 
   const [isLostConnection, setIsLostConnection] = useState(false);
 
+  const queryClient = useQueryClient();
+
+  const handleMessage = useCallback(
+    (msg: WebSocketMessage) => {
+      console.log(msg);
+      switch (msg.type) {
+        case 'TIMER_CREATED': {
+          const timer = msg.data as Timer;
+          queryClient.setQueryData<AxiosResponse<{ data: Timer }>>(
+            [`/team/${params.teamId}/timer/${timer.id}`],
+            (prev) => prev && { ...prev, data: { data: timer } },
+          );
+          queryClient.setQueryData<AxiosResponse<{ data: Timer[] }>>(
+            [`/team/${params.teamId}/timer/list`],
+            (prev) =>
+              prev && { ...prev, data: { data: [...prev.data.data, timer] } },
+          );
+          break;
+        }
+
+        case 'TIMER_DELETED': {
+          const timerId = msg.data as string;
+          queryClient.setQueryData<AxiosResponse<{ data: Timer }>>(
+            [`/team/${params.teamId}/timer/${timerId}`],
+            undefined,
+          );
+          queryClient.setQueryData<AxiosResponse<{ data: Timer[] }>>(
+            [`/team/${params.teamId}/timer/list`],
+            (prev) =>
+              prev && {
+                ...prev,
+                data: {
+                  data: prev.data.data.filter((timer) => timer.id !== timerId),
+                },
+              },
+          );
+          break;
+        }
+
+        case 'TIMER_TICK': // fallthrough
+        case 'TIMER_PAUSED': // fallthrough
+        case 'TIMER_STARTED': {
+          const timer = msg.data as Timer;
+          queryClient.setQueryData<AxiosResponse<{ data: Timer }>>(
+            [`/team/${params.teamId}/timer/${timer.id}`],
+            (prev) => prev && { ...prev, data: { data: timer } },
+          );
+          queryClient.setQueryData<AxiosResponse<{ data: Timer[] }>>(
+            [`/team/${params.teamId}/timer/list`],
+            (prev) =>
+              prev && {
+                ...prev,
+                data: {
+                  data: prev.data.data.map((prevTimer) =>
+                    prevTimer.id === timer.id ? timer : prevTimer,
+                  ),
+                },
+              },
+          );
+          break;
+        }
+
+        default:
+          throw new Error(`Unknown message type: ${msg.type as string}`);
+      }
+    },
+    [params.teamId, queryClient],
+  );
+
   const connect = useCallback(
     (teamId: string) => {
       const newConn = new WebSocket(`ws://localhost:3000/${teamId}`);
 
-      newConn.onmessage = props.onMessage;
+      newConn.onmessage = ({ data }) => {
+        const msg = JSON.parse(data as string) as WebSocketMessage;
+        handleMessage(msg);
+      };
 
       newConn.onopen = () => {
         retryCountRef.current = 0; // 연결 성공 시 재시도 횟수 초기화
@@ -44,7 +125,7 @@ export const WebSocketProvider = (props: Props) => {
 
       return newConn;
     },
-    [props.onMessage],
+    [handleMessage],
   );
 
   useEffect(() => {
