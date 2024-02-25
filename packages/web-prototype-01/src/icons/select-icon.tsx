46e01e776bf9c445785cb3a351b59913bafd7b2b
ryumforcode <ryumforart@gmail.com>
2024-02-26 01:27:03 +0900

    feat: Icons added

---
+++ packages/web-prototype-01/src/icons/select-icon.tsx
@@ -0,0 +1,16 @@
+export const SelectIcon = ({ fill }: { fill: string }) => {
+  return (
+    <svg
+      width="14"
+      height="17"
+      viewBox="0 0 14 17"
+      fill={fill}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6.06674 1.42467C6.3956 0.570286 7.6044 0.570285 7.93326 1.42467L13.405 15.6408C13.6572 16.2958 13.1737 17 12.4718 17H1.52821C0.826328 17 0.342833 16.2958 0.594957 15.6408L6.06674 1.42467Z"
+        fill={fill}
+      />
+    </svg>
+  );
+};
